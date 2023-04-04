211f1b56f40621fca7c2b4ee393290e8da3f300b
EdwardKuchumov <edwardkuchumov@gmail.com>
2023-04-04 18:01:55 +0500

    first-commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// ==================================================burger-toggle
+const burgerBtn = document.querySelector(".header__burger");
+const burgerHrest = document.querySelector(".header__burger-link");
+const modalBurger = document.querySelector(".header__burger-modal");
+burgerBtn.addEventListener("click", function () {
+  modalBurger.classList.toggle("toggle");
+  if (modalBurger.classList.contains("toggle")) {
+    burgerHrest.classList.remove("active");
+  } else {
+    burgerHrest.classList.add("active");
+  }
+});
+
+// ==================================================Slider-Swiper
+const swiper = new Swiper('.swiper-asic', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    }
+    // And if we need scrollbar
+
+  });
